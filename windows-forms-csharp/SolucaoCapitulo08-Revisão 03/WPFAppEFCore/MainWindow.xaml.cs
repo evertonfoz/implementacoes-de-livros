0c66a450abda798109495703c339821cab10cf2f
evertonfoz <evertoncoimbra@gmail.com>
2020-02-21 16:47:19 -0300

    [c#]-20022020_1809

---
+++ windows-forms-csharp/SolucaoCapitulo08-Revisão 03/WPFAppEFCore/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFAppEFCore.Contexts;
+using WPFAppEFCore.POCO;
+
+namespace WPFAppEFCore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            RefreshDataGrid();
+        }
+
+        private IList<Estado> GetEstados()
+        {
+            using (var context = new EFContext())
+            {
+                return context.Estados.ToList<Estado>();
+            }
+        }
+        private void RefreshDataGrid()
+        {
+            dgEstados.ItemsSource = GetEstados();
+        }
+                private Estado SaveEstado(Estado estado)
+        {
+            using (var context = new EFContext())
+            {
+                context.Estados.Add(estado);
+                context.SaveChanges();
+            }
+            return estado;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+                var estado = SaveEstado(new Estado()
+                {
+                    UF = txtUF.Text,
+                    Nome = txtNome.Text
+                });
+                RefreshDataGrid();
+        }
+    }
+}
---
+++ windows-forms-csharp/SolucaoCapitulo08-Revisão 03/WPFAppEFCore/POCO/Estado.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace WPFAppEFCore.POCO
+{
+    public class Estado
+    {
+        public long Id { get; set; }
+        public string UF { get; set; }
+        public string Nome { get; set; }
+        public virtual List<Cidade> Cidades { get; set; }
+        public override string ToString()
+        {
+            return this.Nome;
+        }
+    }
+}
