63f29a0abc29bbd49cede8e710c75ff43989f308
evertonfoz <evertoncoimbra@gmail.com>
2020-05-03 15:35:49 -0300

    [#flutter]-03052020_1535

--- flutter/cc_11/android/app/src/main/kotlin/br/com/casadocodigoflutter/cc11/MainActivity.kt
+++ flutter/cc_11/android/app/src/main/kotlin/br/com/casadocodigoflutter/cc11/MainActivity.kt
@@ -2,5 +2,8 @@ package br.com.casadocodigoflutter.cc11
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
