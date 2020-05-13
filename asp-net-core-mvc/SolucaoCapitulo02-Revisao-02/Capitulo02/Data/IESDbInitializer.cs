e12ac3e53af15521a3424a5af671a23a6b364aaf
evertonfoz <evertoncoimbra@gmail.com>
2020-05-13 09:49:11 -0300

    [#apsnetcore]-fechamento-revisao-02-capiulo02

---
+++ asp-net-core-mvc/SolucaoCapitulo02-Revisao-02/Capitulo02/Data/IESDbInitializer.cs
@@ -0,0 +1,31 @@
+﻿using Capitulo02.Models;
+using System.Linq;
+
+namespace Capitulo02.Data
+{
+    public class IESDbInitializer
+    {
+        public static void Initialize(IESContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            if (context.Departamentos.Any())
+            {
+                return;
+            }
+
+            var departamentos = new Departamento[]
+            {
+                new Departamento { Nome="Ciência da Computação"},
+                new Departamento { Nome="Ciência de Alimentos"}
+            };
+
+            foreach (Departamento d in departamentos)
+            {
+                context.Departamentos.Add(d);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ asp-net-core-mvc/SolucaoCapitulo02-Revisao-02/Capitulo02/Models/Instituicao.cs
@@ -0,0 +1,9 @@
+﻿namespace Capitulo02.Models
+{
+    public class Instituicao
+    {
+        public long? InstituicaoID { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+    }
+}
