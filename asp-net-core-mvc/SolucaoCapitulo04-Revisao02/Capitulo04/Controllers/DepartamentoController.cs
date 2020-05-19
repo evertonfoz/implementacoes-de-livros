a77d1f4b8d0736818367ca3d447f2a60147b287f
evertonfoz <evertoncoimbra@gmail.com>
2020-05-19 12:32:44 -0300

    [#aspnetcore]-19052020_1231

---
+++ asp-net-core-mvc/SolucaoCapitulo04-Revisao02/Capitulo04/Controllers/DepartamentoController.cs
@@ -0,0 +1,21 @@
+﻿using Capitulo04.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capitulo04.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        private readonly IESContext _context;
+        public DepartamentoController(IESContext context)
+        {
+            this._context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Departamentos.Include(i => i.Instituicao).OrderBy(c => c.Nome).ToListAsync());
+        }
+    }
+}
---
+++ asp-net-core-mvc/SolucaoCapitulo04-Revisao02/Capitulo04/Models/Departamento.cs
@@ -0,0 +1,11 @@
+﻿namespace Capitulo04.Models
+{
+    public class Departamento
+    {
+        public long? DepartamentoID { get; set; }
+        public string Nome { get; set; }
+
+        public long? InstituicaoID { get; set; }
+        public Instituicao Instituicao { get; set; }
+    }
+}
