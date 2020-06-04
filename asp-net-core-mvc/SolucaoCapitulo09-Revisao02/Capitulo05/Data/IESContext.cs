26ca251de547b0bb6cd8e010cb2fed6a61b7600f
evertonfoz <evertoncoimbra@gmail.com>
2020-06-04 12:18:14 -0300

    [#aspnetcore]-04062020_1217

---
+++ asp-net-core-mvc/SolucaoCapitulo09-Revisao02/Capitulo05/Data/IESContext.cs
@@ -0,0 +1,38 @@
+﻿using Capitulo05.Models.Infra;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Modelo.Cadastros;
+using Modelo.Discente;
+
+namespace Capitulo05.Data
+{
+    public class IESContext : IdentityDbContext<UsuarioDaAplicacao>
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Instituicao> Instituicoes { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+        public DbSet<Academico> Academicos { get; set; }
+
+        public IESContext(DbContextOptions<IESContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Departamento>().ToTable("Departamento");
+            modelBuilder.Entity<CursoDisciplina>()
+                .HasKey(cd => new { cd.CursoID, cd.DisciplinaID });
+
+            modelBuilder.Entity<CursoDisciplina>()
+                .HasOne(c => c.Curso)
+                .WithMany(cd => cd.CursosDisciplinas)
+                .HasForeignKey(c => c.CursoID);
+
+            modelBuilder.Entity<CursoDisciplina>()
+                .HasOne(d => d.Disciplina)
+                .WithMany(cd => cd.CursosDisciplinas)
+                .HasForeignKey(d => d.DisciplinaID);
+        }
+    }
+}
---
+++ asp-net-core-mvc/SolucaoCapitulo09-Revisao02/Capitulo05/Migrations/20200602182722_CursoDisciplina.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Capitulo05.Migrations
+{
+    public partial class CursoDisciplina : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cursos",
+                columns: table => new
+                {
+                    CursoID = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    DepartamentoID = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cursos", x => x.CursoID);
+                    table.ForeignKey(
+                        name: "FK_Cursos_Departamento_DepartamentoID",
+                        column: x => x.DepartamentoID,
+                        principalTable: "Departamento",
+                        principalColumn: "DepartamentoID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Disciplinas",
+                columns: table => new
+                {
+                    DisciplinaID = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Disciplinas", x => x.DisciplinaID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CursoDisciplina",
+                columns: table => new
+                {
+                    CursoID = table.Column<long>(nullable: false),
+                    DisciplinaID = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CursoDisciplina", x => new { x.CursoID, x.DisciplinaID });
+                    table.ForeignKey(
+                        name: "FK_CursoDisciplina_Cursos_CursoID",
+                        column: x => x.CursoID,
+                        principalTable: "Cursos",
+                        principalColumn: "CursoID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CursoDisciplina_Disciplinas_DisciplinaID",
+                        column: x => x.DisciplinaID,
+                        principalTable: "Disciplinas",
+                        principalColumn: "DisciplinaID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CursoDisciplina_DisciplinaID",
+                table: "CursoDisciplina",
+                column: "DisciplinaID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cursos_DepartamentoID",
+                table: "Cursos",
+                column: "DepartamentoID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CursoDisciplina");
+
+            migrationBuilder.DropTable(
+                name: "Cursos");
+
+            migrationBuilder.DropTable(
+                name: "Disciplinas");
+        }
+    }
+}
