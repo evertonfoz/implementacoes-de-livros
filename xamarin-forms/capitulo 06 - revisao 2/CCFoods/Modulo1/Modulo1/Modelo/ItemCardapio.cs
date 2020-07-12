f747b19b8125da5d3919508efb4577d7576ab69a
evertonfoz <evertoncoimbra@gmail.com>
2020-07-12 17:39:33 -0300

    [#xamarin-1]-12072020_1739

---
+++ xamarin-forms/capitulo 06 - revisao 2/CCFoods/Modulo1/Modulo1/Modelo/ItemCardapio.cs
@@ -0,0 +1,37 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Modulo1.Modelo
+{
+    public class ItemCardapio
+    {
+        [PrimaryKey, AutoIncrement]
+        public long? ItemCardapioId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+        public byte[] Foto { get; set; }
+
+        [ForeignKey(typeof(TipoItemCardapio))]
+        public long? TipoItemCardapioId { get; set; }
+
+        [ManyToOne(CascadeOperations = CascadeOperation.CascadeRead)]
+        public TipoItemCardapio TipoItemCardapio { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            ItemCardapio itemCardapio = obj as ItemCardapio;
+            if (itemCardapio == null)
+            {
+                return false;
+            }
+
+            return (ItemCardapioId.Equals(itemCardapio.ItemCardapioId));
+        }
+
+        public override int GetHashCode()
+        {
+            return ItemCardapioId.GetHashCode();
+        }
+    }
+}
---
+++ xamarin-forms/capitulo 06 - revisao 2/CCFoods/Modulo1/Modulo1/Paginas/TiposItensCardapio/TiposItensCardapioEditPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using Modulo1.Dal;
+using Modulo1.Modelo;
+using Plugin.Media;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Modulo1.Paginas.TiposItensCardapio
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TiposItensCardapioEditPage : ContentPage
+    {
+        private TipoItemCardapio tipoItemCardapio;
+        private string caminhoArquivo;
+        private TipoItemCardapioDAL dalTiposItensCardapio = new TipoItemCardapioDAL();
+
+        public TiposItensCardapioEditPage(TipoItemCardapio tipoItemCardapio)
+        {
+            InitializeComponent();
+            PopularFormulario(tipoItemCardapio);
+            RegistraClickBotaoCamera(idtipoitemcardapio.Text.Trim());
+            RegistraClickBotaoAlbum(idtipoitemcardapio.Text.Trim());
+        }
+
+        private void PopularFormulario(TipoItemCardapio tipoItemCardapio)
+        {
+            this.tipoItemCardapio = tipoItemCardapio;
+            idtipoitemcardapio.Text = tipoItemCardapio.TipoItemCardapioId.ToString();
+            nome.Text = tipoItemCardapio.Nome;
+            caminhoArquivo = tipoItemCardapio.CaminhoArquivoFoto;
+            fototipoitemcardapio.Source = ImageSource.FromFile(tipoItemCardapio.CaminhoArquivoFoto);
+        }
+
+        private void RegistraClickBotaoAlbum(string idparafoto)
+        {
+            BtnAlbum.Clicked += async (sender, args) =>
+            {
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsPickPhotoSupported)
+                {
+                    await DisplayAlert("Álbum não suportado", "Não existe permissão para acessar o álbum de fotos", "OK");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.PickPhotoAsync();
+
+                if (file == null)
+                    return;
+
+                string nomeArquivo = "tipoitem_" + idparafoto + ".jpg";
+                caminhoArquivo = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Fotos");
+
+                if (!Directory.Exists(caminhoArquivo))
+                    Directory.CreateDirectory(caminhoArquivo);
+
+                caminhoArquivo = Path.Combine(caminhoArquivo, nomeArquivo);
+
+                using (FileStream fileStream = new FileStream(caminhoArquivo, FileMode.Create))
+                {
+                    file.GetStream().CopyTo(fileStream);
+                }
+
+                fototipoitemcardapio.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    file.Dispose();
+                    return stream;
+                });
+            };
+        }
+
+        private void RegistraClickBotaoCamera(string idparafoto)
+        {
+            BtnCamera.Clicked += async (sender, args) =>
+            {
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await DisplayAlert("Não existe câmera", "A câmera não está disponível.", "OK");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Fotos",
+                    Name = "tipoitem_" + idparafoto.Trim() + ".jpg"
+                });
+
+                if (file == null)
+                    return;
+
+                fototipoitemcardapio.Source = ImageSource.FromFile(file.Path);
+                caminhoArquivo = file.Path;
+            };
+        }
+
+        public async void BtnGravarClick(object sender, EventArgs e)
+        {
+            if (nome.Text.Trim() == string.Empty)
+            {
+                await this.DisplayAlert("Erro", "Você precisa informar o nome para o novo tipo de item do cardápio.", "Ok");
+            }
+            else
+            {
+                this.tipoItemCardapio.Nome = nome.Text;
+                this.tipoItemCardapio.CaminhoArquivoFoto = caminhoArquivo;
+
+                dalTiposItensCardapio.Update(this.tipoItemCardapio);
+                await Navigation.PopModalAsync();
+            }
+        }
+    }
+}
