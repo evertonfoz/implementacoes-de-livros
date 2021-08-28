8a0f613ce988f165be0c6b2759504aec9ae9c57c
evertonfoz <evertoncoimbra@gmail.com>
2021-08-28 12:43:38 -0300

    1243

--- ionic/capitulo04/src/app/pages/tipo-servicos/add-edit/tipo-servicos-add-edit.page.ts
+++ ionic/capitulo04/src/app/pages/tipo-servicos/add-edit/tipo-servicos-add-edit.page.ts
@@ -1,9 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TipoServicosService } from '../../../services/tipo-servicos.service';
+import { OnInit, Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './tipo-servicos-add-edit.page.html'
 })
 export class TipoServicosAddEditPage implements OnInit {
+    public tipoServico;
+    public modoDeEdicao = false;
+    public tiposServicosForm: FormGroup;
+
+    constructor(
+        private route: ActivatedRoute,
+        private tipoServicoService: TipoServicosService,
+        private formBuilder: FormBuilder
+    ) { }
+
     ngOnInit(): void {
+        const id: number = Number(this.route.snapshot.paramMap.get('id'));
+        this.tipoServico = this.tipoServicoService.getById(id);
+
+        this.tiposServicosForm = this.formBuilder.group({
+            id,
+            nome: [this.tipoServico.nome, Validators.required],
+            valor: [this.tipoServico.valor, Validators.required]
+        });
+    }
+
+    iniciarEdicao() {
+        this.modoDeEdicao = true;
     }
 }
---
+++ ionic/capitulo04/src/app/services/tipo-servicos.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TipoServicosService {
+
+    private tiposServicos = [
+        { id: 1, nome: 'Alinhamento', valor: 12.34 },
+        { id: 2, nome: 'Balanceamento', valor: 56.78 },
+        { id: 3, nome: 'Revisão freios', valor: 90.12 },
+        { id: 4, nome: 'Suspensão', valor: 34.56 }
+    ];
+
+    constructor() { }
+
+    getById(id: number) {
+        const tipoServicoSelecionado = this.tiposServicos.filter(
+            tipoServico => tipoServico.id === id
+        );
+        return tipoServicoSelecionado[0];
+    }
+}
