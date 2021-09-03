ba091e22410657108577e3b6fde45d46fec0258d
evertonfoz <evertoncoimbra@gmail.com>
2021-09-03 08:54:49 -0300

    0854

--- ionic/capitulo05/src/app/pages/ordensdeservico/listagem/ordensdeservico-listagem.module.ts
+++ ionic/capitulo05/src/app/pages/ordensdeservico/listagem/ordensdeservico-listagem.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule, Routes } from '@angular/router';
 import { OrdensDeServicoListagemPage } from './ordensdeservico-listagem.page';
 import { OrdensDeServicoListagemPageRoutingModule } from './ordensdeservico-listagem-routing.module';
 
