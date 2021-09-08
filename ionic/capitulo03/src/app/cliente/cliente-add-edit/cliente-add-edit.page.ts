ef8b8135ee403fa8fef20c7ed9678a6818e0efcc
evertonfoz <evertoncoimbra@gmail.com>
2021-09-08 17:58:23 -0300

    1758

--- ionic/capitulo03/src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
+++ ionic/capitulo03/src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cliente-add-edit',
@@ -7,12 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteAddEditPage implements OnInit {
 
-  constructor() { }
+  clienteForm: FormGroup;
+  hasErrors = false;
+  errorsMessage: string[];
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.clienteForm = this.formBuilder.group({
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(50)])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      telefone: ['', Validators.required],
+      renda: ['0', Validators.compose([Validators.required, Validators.min(0)])],
+      nascimento: ['', Validators.required]
+    });
   }
 
-  submit(inputNome, inputEmail) {
-    console.log(inputNome.value + ' / ' + inputEmail.value);
+  submit() {
+    this.errorsMessage = [];
+    if (this.clienteForm.get('nome').hasError('required')) {
+      this.errorsMessage.push('Nome é obrigatório');
+    }
+    if (this.clienteForm.get('email').hasError('required')) {
+      this.errorsMessage.push('Email é obrigatório');
+    }
+    this.hasErrors = this.errorsMessage.length > 0;
   }
 }
