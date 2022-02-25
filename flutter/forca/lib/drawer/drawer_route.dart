b380706a0e148449bdd5b199d47e95befcc551de
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2022-02-25 08:54:14 -0300

    0854

--- flutter/forca/lib/drawer/drawer_route.dart
+++ flutter/forca/lib/drawer/drawer_route.dart
@@ -1,6 +1,9 @@
 import 'package:capitulo03_splashscreen/drawer/widgets/drawerheader_app.dart';
 import 'package:flutter/material.dart';
 
+import 'widgets/drawerbody_app.dart';
+import 'widgets/drawerbodycontent_app.dart';
+
 class DrawerRoute extends StatelessWidget {
   const DrawerRoute({Key? key}) : super(key: key);
 
@@ -18,6 +21,9 @@ class DrawerRoute extends StatelessWidget {
         child: Column(
           children: const <Widget>[
             DrawerHeaderApp(),
+            DrawerBodyApp(
+              child: DrawerBodyContentApp(),
+            ),
           ],
         ),
       ),
