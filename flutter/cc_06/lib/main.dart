962d290a66157b023b95948da737925417c0799b
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2022-03-23 07:41:54 -0300

    0741

---
+++ flutter/cc_06/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'drawer/blocs/drawer_blocs.dart';
+import 'routes/splash_screen_route.dart';
+
+void main() => runApp(const ForcaApp());
+
+class ForcaApp extends StatelessWidget {
+  const ForcaApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => DrawerBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Forca da UTFPR',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          backgroundColor: Colors.green,
+        ),
+        home: const ForcaHomePage(),
+      ),
+    );
+  }
+}
+
+class ForcaHomePage extends StatelessWidget {
+  const ForcaHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashScreenRoute(),
+    );
+  }
+}
