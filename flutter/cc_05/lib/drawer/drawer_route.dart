e67c34d52c38319fb5c268dcf9afd8cfddce0bbe
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2022-06-22 12:08:52 -0300

    1208

---
+++ flutter/cc_05/lib/drawer/drawer_route.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/drawerbody_app.dart';
+import 'widgets/drawerbodycontent_app.dart';
+import 'widgets/drawerheader_app.dart';
+
+class DrawerRoute extends StatelessWidget {
+  const DrawerRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Jogo da Forca",
+        ),
+        centerTitle: true,
+      ),
+      body: Container(),
+      drawer: Drawer(
+        child: Column(
+          children: const <Widget>[
+            DrawerHeaderApp(),
+            DrawerBodyApp(
+              child: DrawerBodyContentApp(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
