f230c091da1d100e4495ed446e94c161c314f94c
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2026-05-03 22:01:47 -0300

    Limpeza: aplica .gitignore para .NET e Flutter, remove arquivos desnecessários do versionamento

---
+++ orientacao-a-objetos-csharp/revisao_04/Capitulo02/PrimeiroProjeto/Instituicao.cs
@@ -0,0 +1,5 @@
+class Instituicao
+{
+    public string Nome { get; set; }
+    public string Endereco { get; set; }
+}
---
+++ orientacao-a-objetos-csharp/revisao_04/Capitulo02/PrimeiroProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿for (int i = 0; i < args.Length; i++)
+{
+    System.Console.WriteLine(args[i]);
+}
+
+
+Instituicao instituicao = new Instituicao();
+Console.Write("Informe o nome da instituição: ");
+instituicao.Nome = Console.ReadLine();
+
+Console.Write("Informe o endereço da instituição: ");
+instituicao.Endereco = Console.ReadLine();
+
+Console.WriteLine("===================================");
+Console.WriteLine($"Obrigado por informar os dados para a {instituicao.Nome}");
+Console.Write("Pressione ENTER para encerrar.");
+Console.ReadLine();
